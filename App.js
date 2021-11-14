aadce0c34aae6cd75a5b9afad6545cc434e60cc6
GNote <Slava_Gudkov@mail.ru>
2021-11-14 16:56:14 +0300

    настройка

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start 2222565656 11111 working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
